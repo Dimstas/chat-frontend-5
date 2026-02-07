72eb654d5effae0b7b2390c87e2cddecd7675e42
zanin <yu.v.zanin@samsmu.ru>
2026-02-07 08:29:29 +0400

    feat: add use query to profile block

--- src/modules/profile/ui/profile-block/profile-block.tsx
+++ src/modules/profile/ui/profile-block/profile-block.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { MAX_PROFILE } from 'modules/profile/shared/utils/profile';
+import { useInfoProfileQuery } from 'modules/profile/api/info.query';
+import { formatTimestamp } from 'modules/profile/shared/utils/date-time';
 import { JSX } from 'react';
 import { AddButton } from '../action-button';
 import { ProfileAvatar } from '../profile-avatar';
@@ -8,31 +9,34 @@ import { ProfileHeader } from '../profile-header';
 import { ProfileInfo } from '../profile-info';
 import { ProfileLayout } from '../profile-layout';
 import { ProfileNotification } from '../profile-notification';
-import { ProfileUploads } from '../profile-uploads';
 import AddIcon from './icons/add.svg';
 import { ProfileBlockProps } from './profile-block.props';
 
 export const ProfileBlock = ({ uid }: ProfileBlockProps): JSX.Element => {
   void uid;
+  const { data, isLoading, isError, error } = useInfoProfileQuery(uid);
+
+  if (isLoading) return <div>Загрузка...</div>;
+  if (isError) return <div>Ошибка: {error.message}</div>;
 
   return (
-    <ProfileLayout header={<ProfileHeader uid={uid} isBlocked={MAX_PROFILE.is_blocked} />}>
+    <ProfileLayout header={<ProfileHeader uid={uid} isBlocked={data?.isBlocked ?? false} />}>
       <ProfileAvatar
-        avatarHref={MAX_PROFILE.avatar_url ?? '/images/profile/default.png'}
-        firstName={MAX_PROFILE.first_name}
-        lastName={MAX_PROFILE.last_name}
-        isOnline={MAX_PROFILE.is_online}
+        avatarHref={data?.avatarUrl ?? '/images/profile/default.png'}
+        firstName={data?.firstName ?? ''}
+        lastName={data?.lastName ?? ''}
+        isOnline={data?.isOnline ?? false}
       />
       <ProfileNotification uid={uid} />
       <ProfileInfo
-        nickname={MAX_PROFILE.nickname}
-        phoneNumber={MAX_PROFILE.phoneNumber}
-        birthDay={MAX_PROFILE.birthDay}
-        about={MAX_PROFILE.about}
+        nickname={data?.nickName ?? ''}
+        phoneNumber={data?.userName}
+        birthDay={formatTimestamp(data?.birthday)}
+        about={data?.additionalInformation}
       />
-      {!MAX_PROFILE.is_in_contact && <AddButton icon={<AddIcon />} label={'Добавить в контакты'} />}
-      {MAX_PROFILE.is_blocked && <AddButton icon={<AddIcon />} label={'Разблокировать'} />}
-      {MAX_PROFILE.has_uploads && <ProfileUploads uid={uid} />}
+      {/* {!MAX_PROFILE.is_in_contact && <AddButton icon={<AddIcon />} label={'Добавить в контакты'} />} */}
+      {data?.isBlocked && <AddButton icon={<AddIcon />} label={'Разблокировать'} />}
+      {/* {MAX_PROFILE.has_uploads && <ProfileUploads uid={uid} />} */}
     </ProfileLayout>
   );
 };
