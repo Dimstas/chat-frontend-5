1a8baab399b777ef594d8db7437b153bafb04f2f
zanin <yu.v.zanin@samsmu.ru>
2026-02-07 08:16:20 +0400

    feat: add info entity

---
+++ src/modules/profile/entity/info.entity.ts
@@ -0,0 +1,19 @@
+export type ProfileInfo = {
+  uid: string;
+  isDeleted: boolean;
+  userName: string;
+  nickName: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  avatarUrl: string;
+  avatarWebp: string;
+  avatarWebpUrl: string;
+  isFilled: boolean;
+  additionalInformation: string;
+  birthday: number;
+  isOnline: boolean;
+  wasOnlineAt: number;
+  isBlocked: boolean;
+  chatId: number;
+};
