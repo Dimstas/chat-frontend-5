482a50f6c75574b926b2895010db39ab09104bcb
Serhey778 <muha_778sergey@mail.ru>
2026-02-12 17:10:02 +0300

    merge

--- src/app/(protected)/()/@info/chats/[user_uid]/info/page.tsx
+++ src/app/(protected)/()/@info/chats/[user_uid]/info/page.tsx
@@ -1,11 +1,10 @@
-// import Link from 'next/link';
-// import { getChatById } from 'modules/conversation/messages-chat/utils/get-chat-by-id';
-// import { ProfileBlock } from 'modules/profile';
-// import { JSX } from 'react';
+import { getChatById } from 'modules/conversation/messages-chat/utils/get-chat-by-id';
+//import { ProfileBlock } from 'modules/profile';
+import { JSX } from 'react';
 
-// export default async function InfoPage({ params }: { params: Promise<{ user_uid: string }> }): Promise<JSX.Element> {
-//   const user_uid = (await params).user_uid;
-//   const { chat } = getChatById(user_uid)[0];
+export default async function InfoPage({ params }: { params: Promise<{ user_uid: string }> }): Promise<JSX.Element> {
+  const user_uid = (await params).user_uid;
+  const { chat } = getChatById(user_uid)[0];
 
-//   return <ProfileBlock chat={chat} />;
-// }
+  return <></>;
+}
