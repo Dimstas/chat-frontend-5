dd9f0e041c7f62a5f46dacbdae0cc415c049bdb4
zanin <yu.v.zanin@samsmu.ru>
2026-02-12 14:02:53 +0400

    fix: layout when info page open

--- src/layouts/protected-layout/protected-layout.tsx
+++ src/layouts/protected-layout/protected-layout.tsx
@@ -1,9 +1,15 @@
+'use client';
+
+import clsx from 'clsx';
+import { useChatStore } from 'modules/conversation/chats/model/chat.store';
 import { JSX } from 'react';
 import { Header } from './header';
 import styles from './protected-layout.module.scss';
 import { ProtectedLayoutProps } from './protected-layout.props';
 
 export const ProtectedLayout = ({ nav, list, main, info }: ProtectedLayoutProps): JSX.Element => {
+  const { selected } = useChatStore();
+
   return (
     <div className={styles.root}>
       <Header />
@@ -13,8 +19,8 @@ export const ProtectedLayout = ({ nav, list, main, info }: ProtectedLayoutProps)
           <aside className={styles.nav}>{nav}</aside>
           <aside className={styles.list}>{list}</aside>
           <main className={styles.main}>{main}</main>
-          <div>{info}</div>
         </div>
+        <div className={clsx(styles.info, { [styles.open]: !!selected })}>{info}</div>
       </div>
     </div>
   );
