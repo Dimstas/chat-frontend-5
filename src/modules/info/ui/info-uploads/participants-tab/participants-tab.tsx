853b2f7b8755c996ffcc92b651048b07dd95ebbf
zanin <yu.v.zanin@samsmu.ru>
2026-04-07 12:09:22 +0400

    feat: make layout participants tab

--- src/modules/info/ui/info-uploads/participants-tab/participants-tab.tsx
+++ src/modules/info/ui/info-uploads/participants-tab/participants-tab.tsx
@@ -1,27 +1,50 @@
+import { ContactCardSelectable } from 'modules/conversation/contacts/features/contacts-selection';
 import { useParticipantsScreen } from 'modules/info/screens/use-participant-screen';
+import { ParticipantsPanel } from 'modules/info/widgets/participant-panel';
 import { JSX } from 'react';
 import AddIcon from '../../../shared/icons/add.svg';
 import { ActionButton } from '../../action-button';
+import styles from './participants-tab.module.scss';
 import { ParticipantsTabProps } from './participants-tab.props';
+import { SearchPanel } from './search-panel';
 
 export const ParticipantsTab = ({ currentUid, chatKey }: ParticipantsTabProps): JSX.Element => {
-  const { query, setQuery, clearQuery, participants } = useParticipantsScreen(chatKey);
+  const { query, setQuery, clearQuery, participants, filtered } = useParticipantsScreen(chatKey);
 
-  const owner = participants?.find((p) => p.isOwner);
-  const members = participants?.filter((p) => !p.isOwner);
+  const owner = filtered?.find((p) => p.isOwner);
+  const members = filtered?.filter((p) => !p.isOwner) ?? [];
   const current = participants?.find((p) => p.uid === currentUid);
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       {current && <ActionButton icon={<AddIcon />} label={'Пригласить в группу'} onClick={() => {}} />}
-      <div>Владелец</div>
-      <div>{`${owner?.firstName} ${owner?.lastName}`}</div>
-      <div>Участники</div>
-      <ul>
-        {members?.map((p) => (
-          <div key={p.uid}>{`${p.firstName} ${p.lastName}`}</div>
-        ))}
-      </ul>
+      <SearchPanel query={query} onChange={setQuery} onClear={clearQuery} />
+      {query ? (
+        <>
+          <ParticipantsPanel participants={filtered} />
+        </>
+      ) : (
+        <>
+          <div className={styles.label}>Владелец</div>
+          <ContactCardSelectable
+            phone={''}
+            nickname={''}
+            fullName={`${owner?.firstName} ${owner?.lastName}`}
+            uid={owner?.uid ?? ''}
+            firstName={owner?.firstName ?? ''}
+            lastName={owner?.lastName ?? ''}
+            avatarUrl={owner?.avatarUrl ?? ''}
+            isOnline={owner?.isOnline ?? false}
+            wasOnlineAt={owner?.wasOnlineAt ?? 0}
+          />
+          {members.length > 0 && (
+            <>
+              <div className={styles.label}>Участники</div>
+              <ParticipantsPanel participants={members} />
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 };
