7a430c36b216f3201abc3e825b0e42fccc87584b
zanin <yu.v.zanin@samsmu.ru>
2026-04-09 17:49:35 +0400

    feat: add image wrapper to avatar

--- src/modules/info/ui/info-avatar/info-avatar.tsx
+++ src/modules/info/ui/info-avatar/info-avatar.tsx
@@ -6,7 +6,9 @@ import { InfoAvatarProps } from './info-avatar.props';
 export const InfoAvatar = ({ avatarHref, label, status }: InfoAvatarProps): JSX.Element => {
   return (
     <div className={styles.container}>
-      <ImageUI src={avatarHref} alt={label} width={360} height={360} />
+      <div className={styles.imageWrapper}>
+        <ImageUI src={avatarHref} alt={label} width={360} height={360} />
+      </div>
       <div className={styles.imageLabel}>
         <div className={styles.name}>{label}</div>
         <div className={styles.status}>{status}</div>
