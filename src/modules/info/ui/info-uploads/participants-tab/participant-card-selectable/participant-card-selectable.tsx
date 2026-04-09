67a6e285e912c8783eaca8c276cdcb2f63664f39
zanin <yu.v.zanin@samsmu.ru>
2026-04-09 17:20:29 +0400

    feat: add context menu to participant card

--- src/modules/info/ui/info-uploads/participants-tab/participant-card-selectable/participant-card-selectable.tsx
+++ src/modules/info/ui/info-uploads/participants-tab/participant-card-selectable/participant-card-selectable.tsx
@@ -4,16 +4,29 @@ import { ContactCard } from 'modules/conversation/contacts/entity';
 import { Participant } from 'modules/info/entity/info.entity';
 import { useInfoStore } from 'modules/info/model/info.store';
 import { JSX } from 'react';
+import { Dropdown } from 'shared/ui/dropdown';
+import { DropdownItem } from 'shared/ui/dropdown/dropdown.props';
+import DeleteIcon from '../../../../shared/icons/delete-outline.svg';
 
 export const ParticipantCardSelectable = (participant: Participant): JSX.Element => {
   const isSelectionMode = useInfoStore((s) => s.isAddMembersMode);
   const selectedIds = useInfoStore((s) => s.selectedIds);
   const toggleSelection = useInfoStore((s) => s.toggleSelection);
+  const openDeleteModal = useInfoStore((s) => s.openDeleteParticipantModal);
 
   const isSelected = selectedIds.has(participant.uid);
   const { uid, firstName, lastName, avatarUrl, wasOnlineAt, isOnline } = participant;
 
-  return (
+  const contextMenuItems: DropdownItem[] = [
+    {
+      label: 'Удалить',
+      icon: <DeleteIcon />,
+      variant: 'alert',
+      onClick: () => openDeleteModal(participant.uid),
+    },
+  ];
+
+  const contactCard = (
     <ContactCard
       selectionMode={isSelectionMode}
       selected={isSelected}
@@ -31,4 +44,6 @@ export const ParticipantCardSelectable = (participant: Participant): JSX.Element
       }}
     />
   );
+
+  return <>{isSelectionMode ? contactCard : <Dropdown items={contextMenuItems}>{contactCard}</Dropdown>}</>;
 };
